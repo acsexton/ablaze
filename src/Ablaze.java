256afb7a4a2188a20dea71214af8af5ac8349571
AC Sexton <hello@acsexton.com>
2020-07-06 14:29:05 -0400

    factor out item placement

--- src/Ablaze.java
+++ src/Ablaze.java
@@ -5,73 +5,51 @@ import java.util.ArrayList;
  */
 public class Ablaze {
 
-   /** Sensor for use in base simulation */
-   private static SimulatedSensor sensor;
+   // UI
+   private static UI ui;
+
+   // Room
+   private static Room room;
+
+   // Sensor location
+   private static Sensor sensor;
+
+   // List of flammable items
+   private static final ArrayList<Point> flammablePoints = new ArrayList<>();
 
    /**
     * Method in charge of building a basic room with a fire and a sensor (with set locations).
     * @return a newly instantiated Room
     */
-   public static Room buildRoomWithSensor(int[] roomSize, int[] sensorCoords) {
+   public static void buildRoom(UI ui) {
+      // Get room size
+      int[] roomSize = ui.selectRoomSize();
+
       // Room locations count from 0
       String roomName = "basicroom";
       int rows = roomSize[0];
       int columns = roomSize[1];
-      int numOfFires = 1;
+      int numOfFires = 1;  // TODO: Remove this
 
-      // Sensor location (also from 0)
+      room = new Room(roomName, rows, columns, numOfFires);
+   }
+
+   /**
+    * Request and place items in the current room
+    */
+   public static void fillRoomWithStuff() {
+
+      // Place Sensor
+      int[] sensorCoords = ui.selectPointForSensor();
       int sensorRow = sensorCoords[0];
       int sensorColumn = sensorCoords[1];
 
-      Room testRoom = new Room(roomName, rows, columns, numOfFires);
       sensor = new SimulatedSensor();
-      testRoom.placeItemInRoomAtCoords(sensor, sensorRow, sensorColumn);
-      return testRoom;
-   } // end buildRoomWithSensor()
-
-   /**
-    * Runs the simulation for a supplied room.
-    * @param testRoom - the Room on which to run the simulation
-    */
-   public static void testSimWithChair(Room testRoom, ArrayList<Point> points) {
-      int turns = 0;
-
-      while (!sensor.isAlarmed()) {
-         turns++;
-
-         for (Point point : points) {
-            FlammableItem item = (FlammableItem) point.getContainedItem();
-            // Wait a few turns
-            if (turns == item.getTurnsBeforeIgnition()) {
-               // Set the point hot enough to light the item
-               point.setCurrentTemp(item.getCombustionThreshold());
-            } // end if
-         }
-         testRoom.update();
-         System.out.println(testRoom);
-      } // end while
-   } // end testSimWithChair()
-
-   /**
-    * Main entry method. Runs a basic simulation with set locations.
-    * @param args - any supplied arguments (unused)
-    */
-   public static void main(String[] args) {
-      ConsoleUI ui = new ConsoleUI();
-
-      // Get room size
-      int[] roomSize = ui.selectRoomSize();
-
-      // Get sensor location and build room
-      int[] sensorCoords = ui.selectPointForSensor();
-      Room testRoom = buildRoomWithSensor(roomSize, sensorCoords);
-
-      // List of flammable items
-      ArrayList<Point> flammablePoints = new ArrayList<>();
+      room.placeItemInRoomAtCoords((WorldItem) sensor, sensorRow, sensorColumn);
 
       int itemCounter = 0;
-
       int numItemsToPlace = ui.selectNumItemsToPlace();
+
       while (itemCounter < numItemsToPlace) {
 
          int[] itemCoords = ui.selectPointForItem();
@@ -81,18 +59,50 @@ public class Ablaze {
          if (ui.setItemFlammable()){
             int turns = ui.selectNumOfTurnsBeforeIgnition();
             FlammableItem flammable = new FlammableItem("Chair", turns);
-            testRoom.placeItemInRoomAtCoords(flammable, row, column);
-            flammablePoints.add(testRoom.getPointAtLocation(row, column));
+            room.placeItemInRoomAtCoords(flammable, row, column);
+            flammablePoints.add(room.getPointAtLocation(row, column));
          } else {
             NonFlammableItem nonFlam = new NonFlammableItem("Chair");
-            testRoom.placeItemInRoomAtCoords(nonFlam, row, column);
+            room.placeItemInRoomAtCoords(nonFlam, row, column);
          }
 
          // Put a chair in the room
          itemCounter++;
       }
+   }
 
-      testSimWithChair(testRoom, flammablePoints);
+
+   /**
+    * Runs the simulation for a supplied room until sensor is in alarm status
+    */
+   public static void runSimUntilAlarmed() {
+      int turns = 0;
+
+      while (!sensor.isAlarmed()) {
+         turns++;
+
+         for (Point point : flammablePoints) {
+            FlammableItem item = (FlammableItem) point.getContainedItem();
+            // Wait a few turns
+            if (turns == item.getTurnsBeforeIgnition()) {
+               // Set the point hot enough to light the item
+               point.setCurrentTemp(item.getCombustionThreshold());
+            } // end if
+         }
+         room.update();
+         System.out.println(room);
+      } // end while
+   } // end testSimWithChair()
+
+   /**
+    * Main entry method. Runs a basic simulation with set locations.
+    * @param args - any supplied arguments (unused)
+    */
+   public static void main(String[] args) {
+      ui = new ConsoleUI();
+      buildRoom(ui);
+      fillRoomWithStuff();
+      runSimUntilAlarmed();
 
    } // end main()
 
